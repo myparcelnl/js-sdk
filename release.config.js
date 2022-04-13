1eab28816657e948d0b226971bc8c87033ec7c0d
Edie Lemoine <edie@myparcel.nl>
2022-04-13 11:54:33 +0200

    build: separate release config from package.json

---
+++ release.config.js
@@ -0,0 +1,8 @@
+const {addGitHubPlugin, addGitPlugin} = require('@myparcel/semantic-release-config/src/plugins');
+const mainConfig = require('@myparcel/semantic-release-config');
+
+module.exports = {
+  ...mainConfig,
+  extends: '@myparcel/semantic-release-config/npm',
+  plugins: [...mainConfig.plugins, addGitHubPlugin(), addGitPlugin()],
+};
