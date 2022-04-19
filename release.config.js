acf7ca81fbe6a67e4f133db268c84c54a63155e9
Edie Lemoine <edie@myparcel.nl>
2022-04-19 11:42:32 +0200

    build: remove duplicate npm plugin

--- release.config.js
+++ release.config.js
@@ -1,8 +1,8 @@
-const {addGitHubPlugin, addGitPlugin, addNpmPlugin} = require('@myparcel/semantic-release-config/src/plugins');
+const {addGitHubPlugin, addGitPlugin} = require('@myparcel/semantic-release-config/src/plugins');
 const mainConfig = require('@myparcel/semantic-release-config/npm');
 
 module.exports = {
   ...mainConfig,
   extends: '@myparcel/semantic-release-config/npm',
-  plugins: [...mainConfig.plugins, addNpmPlugin(), addGitHubPlugin(), addGitPlugin()],
+  plugins: [...mainConfig.plugins, addGitHubPlugin(), addGitPlugin()],
 };
