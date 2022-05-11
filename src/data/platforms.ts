88148f8e627fe2470e5efd7898c69dcbaa7163cd
Edie Lemoine <edie@myparcel.nl>
2022-05-11 15:18:53 +0200

    fix!: make platforms export consistent with others

--- src/data/platforms.ts
+++ src/data/platforms.ts
@@ -1,33 +1,33 @@
 import {PlatformId, PlatformName} from '@/types/myparcel.types';
 
-export const PLATFORM_MYPARCEL_ID: PlatformId = 1;
-export const PLATFORM_MYPARCEL_NAME: PlatformName = 'myparcel';
-export const PLATFORM_MYPARCEL_HUMAN = 'MyParcel';
+export const MYPARCEL_ID: PlatformId = 1;
+export const MYPARCEL_NAME: PlatformName = 'myparcel';
+export const MYPARCEL_HUMAN = 'MyParcel';
 
-export const PLATFORM_SENDMYPARCEL_ID: PlatformId = 2;
-export const PLATFORM_SENDMYPARCEL_NAME: PlatformName = 'belgie';
-export const PLATFORM_SENDMYPARCEL_HUMAN = 'SendMyParcel';
+export const SENDMYPARCEL_ID: PlatformId = 2;
+export const SENDMYPARCEL_NAME: PlatformName = 'belgie';
+export const SENDMYPARCEL_HUMAN = 'SendMyParcel';
 
-export const PLATFORM_FLESPAKKET_ID: PlatformId = 3;
-export const PLATFORM_FLESPAKKET_NAME: PlatformName = 'flespakket';
-export const PLATFORM_FLESPAKKET_HUMAN = 'Flespakket';
+export const FLESPAKKET_ID: PlatformId = 3;
+export const FLESPAKKET_NAME: PlatformName = 'flespakket';
+export const FLESPAKKET_HUMAN = 'Flespakket';
 
 export const MYPARCEL = {
-  ID: PLATFORM_MYPARCEL_ID,
-  NAME: PLATFORM_MYPARCEL_NAME,
-  HUMAN: PLATFORM_MYPARCEL_HUMAN,
+  ID: MYPARCEL_ID,
+  NAME: MYPARCEL_NAME,
+  HUMAN: MYPARCEL_HUMAN,
 };
 
 export const SENDMYPARCEL = {
-  ID: PLATFORM_SENDMYPARCEL_ID,
-  NAME: PLATFORM_SENDMYPARCEL_NAME,
-  HUMAN: PLATFORM_SENDMYPARCEL_HUMAN,
+  ID: SENDMYPARCEL_ID,
+  NAME: SENDMYPARCEL_NAME,
+  HUMAN: SENDMYPARCEL_HUMAN,
 };
 
 export const FLESPAKKET = {
-  ID: PLATFORM_FLESPAKKET_ID,
-  NAME: PLATFORM_FLESPAKKET_NAME,
-  HUMAN: PLATFORM_FLESPAKKET_HUMAN,
+  ID: FLESPAKKET_ID,
+  NAME: FLESPAKKET_NAME,
+  HUMAN: FLESPAKKET_HUMAN,
 };
 
 export const ALL = [MYPARCEL, SENDMYPARCEL, FLESPAKKET];
