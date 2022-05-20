edb53a07b4b2f28ef4c26e7288065dde902a2557
Edie Lemoine <edie@myparcel.nl>
2022-05-20 16:18:07 +0200

    feat: allow optional path variables
    Resolves #7

---
+++ test/fetch/examples/get-external_integration_provider_credentials-wix.example.ts
@@ -0,0 +1,13 @@
+import {defineMockResponse} from '@Test/fetch/defineMockResponse';
+
+export default defineMockResponse({
+  match: (path: string, init?: RequestInit) => {
+    return init?.method === 'GET' && path === '/external_integration_provider_credentials/wix';
+  },
+
+  response: () => ({
+    data: {
+      credentials: [],
+    },
+  }),
+});
