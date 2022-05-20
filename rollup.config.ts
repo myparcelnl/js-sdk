d388ed225702df17c2c77bf029e27b4d12f503a9
Edie Lemoine <edie@myparcel.nl>
2022-05-20 12:47:59 +0200

    feat: add minified and babeled cjs bundle (#5)

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,15 @@
 import {OutputOptions, ResolveIdHook, defineConfig} from 'rollup';
+import esbuild, {minify} from 'rollup-plugin-esbuild';
 import alias from '@rollup/plugin-alias';
+import {babel} from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import {createAliasEntries} from './private/createAliasEntries';
 import {default as dts} from 'rollup-plugin-dts';
-import esbuild from 'rollup-plugin-esbuild';
 import {getParsedTsConfig} from './private/getParsedTsConfig';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import packageJson from './package.json';
 import path from 'path';
+import typescript from '@rollup/plugin-typescript';
 
 const projectRootDir = path.resolve(__dirname);
 const {paths} = getParsedTsConfig().options;
@@ -15,7 +18,23 @@ const common: OutputOptions = {
   banner: `// ${packageJson.name}@${packageJson.version} ${new Date().toISOString()}`,
 };
 
+const aliasPlugin = alias({
+  customResolver: nodeResolve({extensions: ['.ts']}) as ResolveIdHook,
+  entries: createAliasEntries(paths, projectRootDir),
+});
+
 export default defineConfig([
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        ...common,
+        file: packageJson.module,
+        format: 'esm',
+      },
+    ],
+    plugins: [aliasPlugin, esbuild()],
+  },
   {
     input: './src/index.ts',
     output: [
@@ -27,17 +46,13 @@ export default defineConfig([
       },
       {
         ...common,
-        file: packageJson.module,
-        format: 'esm',
+        file: packageJson.main.replace('.js', '.min.js'),
+        format: 'cjs',
+        plugins: [minify()],
+        sourcemap: true,
       },
     ],
-    plugins: [
-      alias({
-        customResolver: nodeResolve({extensions: ['.tsx', '.ts']}) as ResolveIdHook,
-        entries: createAliasEntries(paths, projectRootDir),
-      }),
-      esbuild(),
-    ],
+    plugins: [aliasPlugin, typescript(), babel({babelHelpers: 'bundled'}), commonjs()],
   },
   {
     input: './src/index.ts',
@@ -51,7 +66,7 @@ export default defineConfig([
     ],
     output: {
       banner: `// ${packageJson.name}@${packageJson.version} ${new Date().toISOString()}`,
-      dir: `dist`,
+      dir: 'dist',
       format: 'esm',
     },
   },
