975bbe9e6ce927efefdf3f37b829b0a10a727406
Edie Lemoine <edie@myparcel.nl>
2022-07-18 13:59:05 +0200

    test: fix type error

--- test/fetch/createFetchMock.ts
+++ test/fetch/createFetchMock.ts
@@ -5,6 +5,8 @@ import {originalFetch} from './originalFetch';
 export const createFetchMock = <T>(implementation?: NoInfer<T>): jest.SpyInstance => {
   originalFetch.set(global.fetch);
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   return jest.spyOn(global, 'fetch').mockImplementation(async (info: RequestInfo, init?: RequestInit) => {
     return Promise.resolve(
       new Response(JSON.stringify(implementation ?? (await getAutoImplementation(info, init))) as BodyInit, init),
