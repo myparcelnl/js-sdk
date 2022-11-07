c7d6d23e827b80f28f278ef19da2390b00f784c8
Edie Lemoine <edie@myparcel.nl>
2022-11-07 17:18:50 +0100

    fix(shipment): add missing header to post /shipments

--- src/endpoints/private/shipments/PostShipments.ts
+++ src/endpoints/private/shipments/PostShipments.ts
@@ -1,4 +1,4 @@
-import {HttpMethod, IdsResponse} from '@/types/request.types';
+import {HttpMethod, IdsResponse, RequestHeaders} from '@/types/request.types';
 import {AbstractPrivateEndpoint} from '@/model/endpoint/AbstractPrivateEndpoint';
 import {CreateDefinition} from '@/model/endpoint/AbstractEndpoint.types';
 import {ShipmentPostData} from '@/endpoints/private/shipments/Shipment.types';
@@ -20,4 +20,11 @@ export class PostShipments extends AbstractPrivateEndpoint<PostShipmentsDefiniti
   public readonly path = 'shipments';
   public readonly property = 'shipments';
   public readonly responseProperty = 'ids';
+
+  public getHeaders(): RequestHeaders {
+    return {
+      ...super.getHeaders(),
+      'Content-Type': 'application/vnd.shipment+json;charset=utf-8;version=1.1',
+    };
+  }
 }
