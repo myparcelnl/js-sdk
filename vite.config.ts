11b74128167e70a1f29640b75f1f1180f23c9521
Edie Lemoine <edie@myparcel.nl>
2023-10-31 11:50:50 +0100

    test: switch back to v8

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,6 @@ const config = defineConfig((env) => {
     },
     test: {
       coverage: {
-        provider: 'istanbul',
         100: true,
         enabled: false,
         reporter: ['text', 'clover', 'html'],
