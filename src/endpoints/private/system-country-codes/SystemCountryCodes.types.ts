5f265e43de4b1f772c7f79f724195f4ef4c96987
Remco Horters <remco.horters@myparcel.nl>
2024-02-06 16:25:05 +0100

    feat: add filters for system country codes

--- src/endpoints/private/system-country-codes/SystemCountryCodes.types.ts
+++ src/endpoints/private/system-country-codes/SystemCountryCodes.types.ts
@@ -1,6 +1,13 @@
+import {type CarrierId} from '@myparcel/constants';
+
 export interface SystemCountryCode {
   label: string;
   region: string;
 }
 
 export type SystemCountryCodesPerCountry = Record<string, SystemCountryCode>[];
+
+export type GetSystemCountryCodesParams = {
+  carrier_id?: CarrierId;
+  unfiltered?: boolean;
+};
