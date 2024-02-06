8fe9e7d5b6ad075d471ba8148287cb5e5ac11b2d
Remco Horters <remco.horters@myparcel.nl>
2024-02-06 16:22:57 +0100

    feat: add put carrier endpoint

--- src/endpoints/private/carrier-options/index.ts
+++ src/endpoints/private/carrier-options/index.ts
@@ -2,3 +2,4 @@ export * from './CarrierOption.types';
 export * from './DeleteCarrierOptions';
 export * from './GetCarrierOptions';
 export * from './PostCarrierOptions';
+export * from './PutCarrierOptions';
